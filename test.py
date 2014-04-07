18a69c0ebee1d0231d5386365c97c75cbeb73553
Eric Lubin <eric@lubin.us>
2014-04-07 12:03:59 -0400

    Got simple simulation working

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import unittest
 
 class TestCase(unittest.TestCase):
     def test_single_simulation(self):
-        s = GameDynamicsWrapper(CWOL, WrightFisher)
+        s = GameDynamicsWrapper(CWOL, WrightFisher, dynamics_kwargs=dict(mu=0.07), game_kwargs=dict())
         s.simulate()
 
 
